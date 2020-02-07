b17e990fb2552bfd5cb218624f5abb642b96321e
luisjedev <luisjedev@gmail.com>
2020-02-07 20:30:30 +0100

    categorias reparadas

--- app/src/main/java/com/example/loggin/dialogoCategoria.java
+++ app/src/main/java/com/example/loggin/dialogoCategoria.java
@@ -6,15 +6,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.DialogFragment;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.sql.SQLOutput;
 
 public class dialogoCategoria extends DialogFragment {
 
@@ -22,9 +30,10 @@ public class dialogoCategoria extends DialogFragment {
     private Button cancelar, aceptar;
     private LinearLayout fondo;
     private DatabaseReference ref;
+    private String[] lista_categorias;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.dialogo_categorias, container, false);
 
@@ -38,6 +47,8 @@ public class dialogoCategoria extends DialogFragment {
 
         comprobarNocheFragment();
 
+        cargarCategorias();
+
         cancelar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -50,8 +61,32 @@ public class dialogoCategoria extends DialogFragment {
             @Override
             public void onClick(View view) {
 
-                ref.child("tienda").child("categorias").push().setValue(categoria.getText().toString());
-                dismiss();
+                if (categoria.getText().toString().equals("")){
+                    Toast.makeText(getContext(), "Nombra la categoría", Toast.LENGTH_SHORT).show();
+                }else{
+
+                boolean existe = false;
+
+                for(int i = 0; i < lista_categorias.length; i++){
+                    if (lista_categorias[i].equals(categoria.getText().toString())) {
+                        System.out.println(lista_categorias[i]);
+                        existe = true;
+                    }
+                }
+
+                System.out.println(existe);
+
+                if (existe) {
+                    Toast.makeText(getContext(), "Ya existe la categoría", Toast.LENGTH_SHORT).show();
+                } else {
+
+
+                    ref.child("tienda").child("categorias").push().setValue(categoria.getText().toString());
+                    dismiss();
+
+                   }
+                }
+
             }
         });
 
@@ -76,7 +111,35 @@ public class dialogoCategoria extends DialogFragment {
         }
     }
 
+    public void cargarCategorias() {
+
+        ref.child("tienda").child("categorias").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if (dataSnapshot.hasChildren()) {
+
+                    int i = 0;
+                    int tamaño = (int) dataSnapshot.getChildrenCount();
+                    lista_categorias = new String[tamaño];
+
+                    for (DataSnapshot hijo : dataSnapshot.getChildren()) {
+
+                        String heroe = hijo.getValue(String.class);
+                        lista_categorias[i] = heroe;
+                        i++;
+
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
 
+    }
 
 }
